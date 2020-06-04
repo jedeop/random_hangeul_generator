6505d05b9271f48f97604f640c6bd0e757c227fb
DQSR <cong0116@naver.com>
2020-06-04 13:37:23 +0900

    basic logic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+<div>
+  <div>
+    <label for="length">길이</label>
+    <input type="number" name="length" id="length" v-model="state.length">
+  </div>
+
+  <div>
+    <label for="jongsung">종성 없이</label>
+    <input type="checkbox" name="jongsung" id="jongsung" v-model="state.removeJongsung">
+  </div>
+
+  <input type="button" name="generate" value="생성하기" @click="generate">
+</div>
+<div>
+  {{ state.result }}
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from 'vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  setup() {
+    const state = reactive({
+      length: 10,
+      removeJongsung: false,
+      result: ''
+    })
+
+    function generate() {
+      let text = '';
+      for (let i = 0; i < state.length; i++) {
+        let code = Math.floor(Math.random() * (0xD7A3 - 0xAC00)) + 0xAC00;
+        if (state.removeJongsung) {
+          let lastChar = (code - 0xAC00) % (21 * 28) % 28;
+          code = code - lastChar;
+        }
+        text += String.fromCharCode(code);
+      }
+      state.result = text
+    }
+
+    return {
+      state,
+      generate
+    }
   }
 }
 </script>
